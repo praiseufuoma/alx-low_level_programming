c44363afdde4f3d86ff6687ce63987f9c103bcbc
root <root@DESKTOP-EKTHL6U>
2024-08-17 18:28:36 +0100

    updated all

--- 0x00-hello_world/0-main.c
+++ 0x00-hello_world/0-main.c
@@ -3,9 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (success)
+ *
  */
 int main(void)
 {
-    return (0);
+	return (0);
 }
--- 0x00-hello_world/me.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int mani (void){
-    printf("HELLOWORLD");
-    return 0;
-}
